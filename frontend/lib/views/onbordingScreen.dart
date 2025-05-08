bf98e71d0c1260655a019d213414a2ef8c58993c
ASEGIDADANE <asegidadane27@gmail.com>
2025-05-08 20:42:10 +0300

    MVVM folder Structure is add

---
+++ frontend/lib/views/onbordingScreen.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class onbordingScreen extends StatelessWidget {
+  const onbordingScreen({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
