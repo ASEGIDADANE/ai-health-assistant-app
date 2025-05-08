510e3743a3edbc6222c857142cda43466a817b43
ASEGIDADANE <asegidadane27@gmail.com>
2025-05-08 18:50:51 +0300

    healthprofile route and gethealthprofile is finished

--- backend/src/controllers/healthprofileControllers.ts
+++ backend/src/controllers/healthprofileControllers.ts
@@ -38,3 +38,28 @@ export const createOrUpdateHealthProfile = async (req: Request, res: Response) :
 };
 
 
+export const getHealthProfile = async (req: Request, res: Response): Promise<void> => {
+    try {
+        if (!req.user || !req.user.id) {
+        res.status(401).json({ message: "Unauthorized: User not authenticated" });
+        return;
+        } 
+        const userId = req.user.id;
+    
+        const healthProfile = await HealthProfile.findOne({ user: userId });
+    
+        if (!healthProfile) {
+        res.status(404).json({ message: "Health profile not found" });
+        return;
+        }
+    
+        res.status(200).json(healthProfile);
+    } catch (error) {
+        console.error("Error fetching health profile:", error);
+        res.status(500).json({ message: "Server error" });
+    }
+    }
+
+    
+
+
--- backend/src/server.ts
+++ backend/src/server.ts
@@ -3,6 +3,7 @@ import dotenv from 'dotenv';
 dotenv.config();
 import connectDB from './config/db';
 import authRoutes from './routes/authRoutes';
+import healthProfileRoutes from './routes/healthProfileRoutes';
 
 
 
@@ -26,6 +27,7 @@ app.use(express.urlencoded({ extended: true }));
 // routes
 
 app.use('/api/auth',authRoutes );
+app.use('/api/health',healthProfileRoutes );
 
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
