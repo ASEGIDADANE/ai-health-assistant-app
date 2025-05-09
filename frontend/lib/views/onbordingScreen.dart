6bbe4b6f548cbc86481535e05e79759a8da8a0ed
ASEGIDADANE <asegidadane27@gmail.com>
2025-05-09 08:58:03 +0300

    health assitance seletion page is finished

--- frontend/lib/views/onbordingScreen.dart
+++ frontend/lib/views/onbordingScreen.dart
@@ -1,10 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:frontend/views/homeScreen.dart';
 
 class onbordingScreen extends StatelessWidget {
   const onbordingScreen({super.key});
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return const homeScreen();
   }
 }
