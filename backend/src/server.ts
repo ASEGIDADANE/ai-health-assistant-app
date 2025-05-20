ffc3875ea6de7ac52178049dea56a66a19a17a55
Israel Sol <isrucrasus10@gmail.com>
2025-05-20 17:42:34 +0300

    resolve server.ts nearby_routes

--- backend/src/server.ts
+++ backend/src/server.ts
@@ -6,17 +6,24 @@ import connectDB from './config/db';
 import authRoutes from './routes/authRoutes';
 import healthProfileRoutes from './routes/healthProfileRoutes';
 import aiRoutes from './routes/aiRoutes';
-import healthRoutes from './routes/healthRoutes'; // Import the router
-
+import healthRoutes from './routes/healthRoutes';
+import nearbyRoutes from './routes/nearbyRoutes'; 
 
 const app = express();
-
-app.use(cors());
-
+const PORT = process.env.PORT || 5000;
 
 //  connect to database
 
 connectDB();
+
+
+app.use(cors({
+  origin: '*', // Allow all origins in development
+  methods: ['GET', 'POST', 'PUT', 'DELETE'],
+  allowedHeaders: ['Content-Type', 'Authorization']
+}));
+
+
 
 
 // middleware
@@ -33,11 +40,14 @@ app.use(express.urlencoded({ extended: true }));
 
 
 // routes
+
+app.use('/api/auth', authRoutes);
+app.use('/api/health', healthProfileRoutes);
+app.use('/api/ai', aiRoutes);
 app.use('/api/healthsnippets', healthRoutes);
-app.use('/api/auth',authRoutes );
-app.use('/api/health',healthProfileRoutes );
-app.use('/api/ai',aiRoutes);
-// healthsnippets/snippet
+app.use('/api/nearby', nearbyRoutes);  
+
+
 app.listen(process.env.PORT, () => {
   console.log(`Server is running on port ${process.env.PORT}`);
 }
